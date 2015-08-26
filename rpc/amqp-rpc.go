719ed855e796f78a3cfbe4c85aa8c1ddeab58293
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-08-26 12:47:31 -0700

    Moving Ack/Reject to end of call not to interrupt return of wrapped errors

--- rpc/amqp-rpc.go
+++ rpc/amqp-rpc.go
@@ -358,16 +358,10 @@ func (rpc *AmqpRPCServer) processMessage(msg amqp.Delivery) {
 	if err != nil {
 		// AUDIT[ Error Conditions ] 9cc4d537-8534-4970-8665-4b382abe82f3
 		rpc.log.Audit(fmt.Sprintf(" [s>][%s][%s] Error condition marshalling RPC response %s [%s]", rpc.serverQueue, msg.ReplyTo, msg.Type, msg.CorrelationId))
-		if !rpc.autoAck {
-			msg.Reject(true) // requeue
-		}
 		return
 	}
 	if response.Error.Value != "" {
 		rpc.log.Info(fmt.Sprintf(" [s>][%s][%s] %s failed, replying: %s (%s) [%s]", rpc.serverQueue, msg.ReplyTo, msg.Type, response.Error.Value, response.Error.Type, msg.CorrelationId))
-		if !rpc.autoAck {
-			msg.Reject(true) // requeue? Not sure if the response queue will work after this is done
-		}
 	}
 	rpc.log.Debug(fmt.Sprintf(" [s>][%s][%s] replying %s(%s) [%s]", rpc.serverQueue, msg.ReplyTo, msg.Type, core.B64enc(jsonResponse), msg.CorrelationId))
 	rpc.Channel.Publish(
@@ -381,7 +375,11 @@ func (rpc *AmqpRPCServer) processMessage(msg amqp.Delivery) {
 			Body:          jsonResponse, // XXX-JWS: jws.Sign(privKey, body)
 		})
 	if !rpc.autoAck {
-		msg.Ack(false) // Only ack the current delivery!
+		if response.Error.Value != "" {
+			msg.Reject(true) // requeue!
+		} else {
+			msg.Ack(false) // only ack the current delivery!
+		}
 	}
 }
 
