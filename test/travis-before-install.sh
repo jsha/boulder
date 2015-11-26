092d0953ae16172b2867359df9cbe8db4b5646c1
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-11-25 23:56:17 -0800

    Symlink before go run.

--- test/travis-before-install.sh
+++ test/travis-before-install.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -o xtrace
 
-# Travis does shallow clones, so there is no master branch present.
-# But test-no-outdated-migrations.sh needs to check diffs against master.
-# Fetch just the master branch from origin.
-( git fetch origin master
-git branch master FETCH_HEAD ) &
-# Github-PR-Status secret
-if [ -n "$encrypted_53b2630f0fb4_key" ]; then
-  openssl aes-256-cbc \
-    -K $encrypted_53b2630f0fb4_key -iv $encrypted_53b2630f0fb4_iv \
-    -in test/github-secret.json.enc -out /tmp/github-secret.json -d
+if [ "${TRAVIS}" == "true" ]; then
+  # Boulder consists of multiple Go packages, which
+  # refer to each other by their absolute GitHub path,
+  # e.g. github.com/letsencrypt/boulder/analysis. That means, by default, if
+  # someone forks the repo, Travis won't pass on their own repo. To fix that,
+  # we add a symlink.
+  mkdir -p $TRAVIS_BUILD_DIR $GOPATH/src/github.com/letsencrypt
+  if [ ! -d $GOPATH/src/github.com/letsencrypt/boulder ] ; then
+    ln -s $TRAVIS_BUILD_DIR $GOPATH/src/github.com/letsencrypt/boulder
+  fi
+
+  # Travis does shallow clones, so there is no master branch present.
+  # But test-no-outdated-migrations.sh needs to check diffs against master.
+  # Fetch just the master branch from origin.
+  ( git fetch origin master
+  git branch master FETCH_HEAD ) &
+  # Github-PR-Status secret
+  if [ -n "$encrypted_53b2630f0fb4_key" ]; then
+    openssl aes-256-cbc \
+      -K $encrypted_53b2630f0fb4_key -iv $encrypted_53b2630f0fb4_iv \
+      -in test/github-secret.json.enc -out /tmp/github-secret.json -d
+  fi
+else
+  alias travis_retry=""
 fi
 
 travis_retry go get \
@@ -32,15 +46,5 @@ go run cmd/rabbitmq-setup/main.go  --config test/boulder-config.json &
 
 # Wait for all the background commands to finish.
 wait
-
-# Boulder consists of multiple Go packages, which
-# refer to each other by their absolute GitHub path,
-# e.g. github.com/letsencrypt/boulder/analysis. That means, by default, if
-# someone forks the repo, Travis won't pass on their own repo. To fix that,
-# we add a symlink.
-mkdir -p $TRAVIS_BUILD_DIR $GOPATH/src/github.com/letsencrypt
-if [ ! -d $GOPATH/src/github.com/letsencrypt/boulder ] ; then
-  ln -s $TRAVIS_BUILD_DIR $GOPATH/src/github.com/letsencrypt/boulder
-fi
 
 set +o xtrace
