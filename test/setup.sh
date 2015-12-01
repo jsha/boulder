a83beabccb30d1bd6013b37f526edd9c6e1c53c0
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-11-30 23:00:13 -0800

    create_db after goose

--- test/setup.sh
+++ test/setup.sh
@@ -12,12 +12,11 @@ go get \
 (wget https://github.com/jsha/boulder-tools/raw/master/goose.gz &&
  mkdir -p $GOPATH/bin &&
  zcat goose.gz > $GOPATH/bin/goose &&
- chmod +x $GOPATH/bin/goose) &
+ chmod +x $GOPATH/bin/goose &&
+ ./test/create_db.sh) &
 
 # Set up rabbitmq exchange and activity monitor queue
 go run cmd/rabbitmq-setup/main.go -server amqp://localhost &
-
-./test/create_db.sh &
 
 # Wait for all the background commands to finish.
 wait
