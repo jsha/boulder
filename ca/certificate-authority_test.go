9b5f2338093742f564336f14468bc658a0af5a45
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-03-21 15:35:37 -0700

    Fix vendorized import.

--- ca/certificate-authority_test.go
+++ ca/certificate-authority_test.go
@@ -16,11 +16,10 @@ import (
 	"testing"
 	"time"
 
-	"golang.org/x/crypto/ocsp"
-
 	cfsslConfig "github.com/letsencrypt/boulder/Godeps/_workspace/src/github.com/cloudflare/cfssl/config"
 	"github.com/letsencrypt/boulder/Godeps/_workspace/src/github.com/cloudflare/cfssl/helpers"
 	"github.com/letsencrypt/boulder/Godeps/_workspace/src/github.com/jmhodges/clock"
+	"github.com/letsencrypt/boulder/Godeps/_workspace/src/golang.org/x/crypto/ocsp"
 
 	"github.com/letsencrypt/boulder/cmd"
 	"github.com/letsencrypt/boulder/core"
