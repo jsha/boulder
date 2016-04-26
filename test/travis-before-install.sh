a4a78f48c9801b22609f8cde854d3b31e599910e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-26 16:31:34 -0700

    CD to right dir before setup.sh

--- test/travis-before-install.sh
+++ test/travis-before-install.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -o xtrace
 
-# Boulder consists of multiple Go packages, which
-# refer to each other by their absolute GitHub path,
-# That means, by default, if someone forks the repo,
-# Travis won't pass on their own repo. To fix that,
-# we add a symlink.
-mkdir -p $TRAVIS_BUILD_DIR $GOPATH/src/github.com/letsencrypt
-if [ ! -d $GOPATH/src/github.com/letsencrypt/boulder ] ; then
-  ln -s $TRAVIS_BUILD_DIR $GOPATH/src/github.com/letsencrypt/boulder
-fi
-
 # Travis does shallow clones, so there is no master branch present.
 # But test-no-outdated-migrations.sh needs to check diffs against master.
 # Fetch just the master branch from origin.
