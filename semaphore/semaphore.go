837e2c804fcd2e348abafc08bef06659edd27902
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2022-11-15 09:29:53 -0800

    wip

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -1,6 +1,7 @@
 // Copyright 2017 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+// Modified by Boulder to provide a load-shedding mechanism.
 
 // Package semaphore provides a weighted semaphore implementation.
 package semaphore // import "golang.org/x/sync/semaphore"
@@ -8,28 +9,34 @@ package semaphore // import "golang.org/x/sync/semaphore"
 import (
 	"container/list"
 	"context"
+	"errors"
 	"sync"
 )
 
 type waiter struct {
-	n     int64
-	ready chan<- struct{} // Closed when semaphore acquired.
+	n          int64
+	maxWaiters int
+	ready      chan<- struct{} // Closed when semaphore acquired.
 }
 
 // NewWeighted creates a new weighted semaphore with the given
 // maximum combined weight for concurrent access.
-func NewWeighted(n int64) *Weighted {
-	w := &Weighted{size: n}
+// maxWaiters provides a limit such that calls to Acquire
+// will immediately error if the number of weighters is that high.
+// A maxWaiters of zero means no limit.
+func NewWeighted(n int64, maxWaiters int) *Weighted {
+	w := &Weighted{size: n, maxWaiters: maxWaiters}
 	return w
 }
 
 // Weighted provides a way to bound concurrent access to a resource.
 // The callers can request access with a given weight.
 type Weighted struct {
-	size    int64
-	cur     int64
-	mu      sync.Mutex
-	waiters list.List
+	size       int64
+	cur        int64
+	mu         sync.Mutex
+	waiters    list.List
+	maxWaiters int
 }
 
 // Acquire acquires the semaphore with a weight of n, blocking until resources
@@ -37,6 +44,8 @@ type Weighted struct {
 // ctx.Err() and leaves the semaphore unchanged.
 //
 // If ctx is already done, Acquire may still succeed without blocking.
+//
+// If there are maxWaiters waiters, Acquire will return an error immediately.
 func (s *Weighted) Acquire(ctx context.Context, n int64) error {
 	s.mu.Lock()
 	if s.size-s.cur >= n && s.waiters.Len() == 0 {
@@ -50,6 +59,10 @@ func (s *Weighted) Acquire(ctx context.Context, n int64) error {
 		s.mu.Unlock()
 		<-ctx.Done()
 		return ctx.Err()
+	}
+
+	if s.maxWaiters > 0 && s.waiters.Len() > s.maxWaiters {
+		return errors.New("too many waiters")
 	}
 
 	ready := make(chan struct{})
